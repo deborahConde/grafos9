c4849ee347c989e524b2eee955a31e227c66d5d2
Pedro <pedrohsareis@gmail.com>
2018-05-18 22:20:20 -0300

    Alteração do nome de algumas variáveis e criação da estrutura de menu

---
+++ Aresta.h
@@ -0,0 +1,21 @@
+#ifndef ARESTA_H_INCLUDED
+#define ARESTA_H_INCLUDED
+
+class Aresta{
+    private:
+        float pesoAresta;
+        int id_aresta;
+        Aresta *proxAresta;
+    public:
+        Aresta(float peso) { pesoAresta = peso;};
+        ~Aresta();
+        float getPesoAresta();
+        int getIDNo();
+        Aresta* getProxAresta() {return proxAresta;};
+        void setPesoAresta(float);
+        void setProxAresta(Aresta* x) {proxAresta = x ; };
+        void setIdNo(int id_aresta) {id_aresta = id_aresta;};
+};
+
+
+#endif // ARESTA_H_INCLUDED
